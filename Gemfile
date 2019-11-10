3dbae4e6c23e83e1d4c9cfe780c15f9eb818c950
25 macamp <rateliasudaisuki@gmail.com>
2019-11-11 00:01:19 +0900

    kaminariでページネーション

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'puma', '~> 3.11'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 gem 'active_model_serializers'
+gem 'kaminari'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+Article.create([
+                 { title: 'Test Title 1', content: 'Test Content 1', slug: 'Test Slug 1' },
+                 { title: 'Test Title 2', content: 'Test Content 2', slug: 'Test Slug 2' },
+                 { title: 'Test Title 3', content: 'Test Content 3', slug: 'Test Slug 3' }
+               ])
