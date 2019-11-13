fed7edf8754b91b1a5ecf4ce3792f943beca1ab7
25 macamp <rateliasudaisuki@gmail.com>
2019-11-13 11:11:04 +0900

    shared_examples_forでリファクタリング

--- spec/requests/access_tokens_spec.rb
+++ spec/requests/access_tokens_spec.rb
@@ -4,27 +4,16 @@ require 'rails_helper'
 
 RSpec.describe 'AccessTokens', type: :request do
   describe '#create' do
-    context 'when invalid request' do
-      let(:authorization_error) do
-        {
-          'status' => '401',
-          'source' => { 'pointer' => '/code' },
-          'title' => 'Authentication Invalid',
-          'detail' => 'You must provide valid code in order to exchange it for token.'
-        }
-      end
+    context 'when no code provided' do
+      subject { post login_path }
 
-      it 'when not code provided' do
-        post login_path
-        expect(response).to have_http_status 401
-        expect(json['errors']).to include authorization_error
-      end
+      it_behaves_like 'authorization_error'
+    end
 
-      it 'when code is invalid' do
-        post login_path, params: { code: 'invalid_code' }
-        expect(response).to have_http_status 401
-        expect(json['errors']).to include(authorization_error)
-      end
+    context 'when invalid code provided' do
+      subject { post login_path, params: { code: 'invalid code' } }
+
+      it_behaves_like 'authorization_error'
     end
 
     context 'when valid requests' do
