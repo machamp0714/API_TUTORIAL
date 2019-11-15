51f907cea231a413e8a9ec66d8e46fbde49d57c5
25 macamp <rateliasudaisuki@gmail.com>
2019-11-15 11:10:23 +0900

    Commentモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_004354) do
+ActiveRecord::Schema.define(version: 2019_11_15_020539) do
 
   create_table "access_tokens", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "token", null: false
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2019_11_15_004354) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "content"
+    t.bigint "article_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "login", null: false
@@ -43,4 +53,6 @@ ActiveRecord::Schema.define(version: 2019_11_15_004354) do
 
   add_foreign_key "access_tokens", "users"
   add_foreign_key "articles", "users"
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
