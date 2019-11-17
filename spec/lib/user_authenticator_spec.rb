96821701a25cd994a9536fd58450ed715ba8139e
25 macamp <rateliasudaisuki@gmail.com>
2019-11-17 23:37:59 +0900

    user_authenticatorのテストを編集

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -3,60 +3,26 @@
 require 'rails_helper'
 
 describe UserAuthenticator do
-  describe '#perform' do
+  context 'when initialized with code' do
     let(:authenticator) { described_class.new(code: 'sample_code') }
-    subject { authenticator.perform }
+    let(:authenticator_class) { UserAuthenticator::Oauth }
 
-    context 'when code is incorrect' do
-      let(:error) { double('Sawyer::Resource', error: 'bad_verification_code') }
-
-      before do
-        allow(Octokit::Client.new).to receive(:exchange_code_for_token).and_return(error)
-      end
-
-      it 'should raise error' do
-        expect { subject }.to raise_error(
-          UserAuthenticator::Oauth::AuthenticationError
-        )
-        expect(authenticator.user).to be_nil
+    describe '#initialize' do
+      it 'should initialize proper authenticator' do
+        expect(authenticator_class).to receive(:new).with('sample_code')
+        authenticator
       end
     end
+  end
 
-    context 'when code is correct' do
-      let(:user_data) do
-        {
-          login: 'sample',
-          url: 'https://sample/url',
-          avatar_url: 'https://sample/avatar/url',
-          name: 'sample_name'
-        }
-      end
+  context 'when initialized with login & password' do
+    let(:authenticator) { described_class.new(login: 'jsmith', password: 'password') }
+    let(:authenticator_class) { UserAuthenticator::Standard }
 
-      before do
-        # allow_any_instance_ofの挙動について理解する
-        allow_any_instance_of(Octokit::Client).to receive(
-          :exchange_code_for_token
-        ).and_return('validaccesstoken')
-
-        allow_any_instance_of(Octokit::Client).to receive(
-          :user
-        ).and_return(user_data)
-      end
-
-      it 'should create a new user when dose not exist' do
-        expect { subject }.to change { User.count }.by(1)
-        expect(User.last.name).to eq user_data[:name]
-      end
-
-      it 'should reuse registered user' do
-        user = create :user, user_data
-        expect { subject }.not_to change { User.count }
-        expect(authenticator.user).to eq(user)
-      end
-
-      it 'should create access token and set token' do
-        expect { subject }.to change { AccessToken.count }.by(1)
-        expect(authenticator.access_token).to be_present
+    describe '#initialize' do
+      it 'should initialize proper authenticator' do
+        expect(authenticator_class).to receive(:new).with('jsmith', 'password')
+        authenticator
       end
     end
   end
